5666854b7af08de75a0062aa0e9101a1036dffc4
jmacj <jm.nz0723@gmail.com>
2018-01-22 09:32:50 +0800

    Changed to dynamic

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -3,47 +3,27 @@
     <div class="row">
         <!-- Forum Entries Column -->
         <div class="col-md-12">
+          <?php foreach($posts as $post): ?>
            <div class="card">
                    <!-- First Forum Post -->
                    <h2 class="card-header">
                        <div class="profile-header">
                            <a href=""> <span><i class="fa fa-user-circle" aria-hidden="true"></i></span></a>
-                           <p class="d-inline-block"><?= $user->first_name . " " . $user->last_name ?><br>
+                           <p class="d-inline-block"> <?=$post['author'] ?><br>
                                <span class="text-muted"> time</span> <i class="fa fa-clock-o" aria-hidden="true"></i></p>
                            
                        </div>
                    </h2>
-                   <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                   <img class="img-responsive" src="http://placehold.it/900x300" alt="<?=$post['title']?>">
                    <div class="conatiner">
-                       <p class="forum-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>                       
+                       <p class="forum-content"><?=$post['body']?></p>                       
                    </div>
                    <div class="card-footer">
-                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                        <a class="btn btn-primary" href="<?=base_url('post/view/' . $post['id'])?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
                    </div>
             </div>
             <br>
-            
-            <div class="card">
-                <!-- First Forum Post -->
-                <h2 class="card-header">
-                    <div class="profile-header">
-                        <a href=""> <span><i class="fa fa-user-circle" aria-hidden="true"></i></span></a>
-                        <p class="d-inline-block"><?= $user->first_name . " " . $user->last_name ?><br>
-                            <span class="text-muted"> time</span> <i class="fa fa-clock-o" aria-hidden="true"></i></p>
-
-                    </div>
-                </h2>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <div class="conatiner">
-                    <p class="forum-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>                       
-                </div>
-                <div class="card-footer">
-                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                </div>
-            </div>
-            
-        </div>
-        <br>
+            <?php endforeach; ?>
     </div>
     <!-- /.row -->
 
