09f80919eba2bf566c98aa1a7054c6b3ae3216b1
shrimpdoom1920 <nathan_siegfrid@yahoo.com>
2018-01-29 15:41:43 +0800

    Card post fixed and form fixed

--- application/views/post/create.php
+++ application/views/post/create.php
@@ -3,29 +3,24 @@
         <div class="col-md-12">
             <div class="card space-between w3-card-4">
                 <div class="card-header">
-                     <p class="d-inline-block message-header">New Thread    </p>
+                     <span class="d-inline-block message-header">New Thread </span>
                 </div>
                 <div class="card-block title-wrapper">
                     <form action="post/create" method="POST" class="message">
                          <div class="form-group comment-form">
-                            <input type="text" name="title" placeholder="Title" class="form-control"></input>
+                            <input type="text" name="title" placeholder="Title" class="form-control" id="create-title"></input>
                          </div>
                     </div>
                     <hr>
-                    <div class="page-wrapper">
+                    <div class="create-wrapper">
                         <textarea name="body" class="form-control" placeholder="Your Message" row="10" id="create-post"></textarea>
+                        <button type="submit" value="Submit" class="btn btn-primary btn-submit float-right" style="margin: 10px;">Submit</button>
                     </div>
-                    <input type="submit" value="Submit" class="btn btn-primary btn-submit float-right">
                 </form>              
             </div>
         </div>
 
-        <script>
-        $(document).ready(function() {
-            $('.content').richText();
-            $('.richText-editor').html();
-        });
-        </script>
+       
             
     </div>
     <!-- /.row -->
--- application/views/post/view.php
+++ application/views/post/view.php
@@ -1,75 +1,75 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="card space-between w3-card-4">
-                <div class="card-header">
-                    <div class="profile-header">
-                        <p class="d-inline-block"><?= $thread['author'] ?></p><br>
-                            <p class="text-muted"><?= $thread['created_at'] ?> <i class="fa fa-clock-o" aria-hidden="true"></i></p>
-
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-2 text-center">
-                            <span><img src="<?= $thread['avatar_url'] ?>" class="img-thumbnail w-50 mt-3" aria-hidden="true"></i></span>
+            <div class="background-forum">
+                <div class="card space-between card-details">
+                    <div class="card-header">
+                        <div class="profile-header">
+                            <span class="d-inline-block font-weight-bold"><?= $thread['author'] ?></span>
+                                <span class="text-muted"><?= $thread['created_at'] ?> <i class="fa fa-clock-o" aria-hidden="true"></i></span>
                         </div>
-                        <div class="col-md-10 right-content border border-top-0 border-bottom-0 border-right-0">
-                            <h1><?= $thread['title'] ?></h1>
-                            <div class="container">
-                                <p class="forum-content">
-                                    <?= $thread['body'] ?>
-                                </p>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-2 text-center">
+                                <span><img src="<?= $thread['avatar_url'] ?>" class="img-thumbnail w-50 mt-3" aria-hidden="true"></i></span>
                             </div>
-                        </div>
-                    </div>                    
-                </div>
-                <div class="card-footer">
-                    <a class="btn btn-primary btn-reply float-right" href="#reply">Reply <span> <i class="fa fa-reply" aria-hidden="true"></i></span></a>
-                </div>
-            </div> 
-
-            <?php foreach ($thread['comment'] as $comment): ?>
-            <div class="card space-between w3-card-4">
-                <div class="card-header">
-                    <div class="profile-header">
-                    <p class="d-inline-block"><?= $thread['author'] ?></p><br>
-                            <p class="text-muted"><?= $comment['created_at'] ?> <i class="fa fa-clock-o" aria-hidden="true"></i></p>
+                            <div class="col-md-10 right-content border border-top-0 border-bottom-0 border-right-0">
+                                <div class="font-weight-bold forum-title "><?= $thread['title'] ?></div>
+                                <div class="container">
+                                    <div class="forum-content">
+                                        <?= $thread['body'] ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                    
                     </div>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-2 text-center">
-                            <span><img src="<?= $thread['avatar_url'] ?>" class="img-thumbnail w-50 mt-3" aria-hidden="true"></i></span>
+                    <div class="card-footer footer-reply">
+                        <a class="btn btn-primary btn-reply float-right" href="#reply">Reply <span> <i class="fa fa-reply" aria-hidden="true"></i></span></a>
+                    </div>
+                </div> 
+
+                <?php foreach ($thread['comment'] as $comment): ?>
+                <div class="card space-between">
+                    <div class="card-header">
+                        <div class="forum-comments">
+                            <span class="text-muted"><?= $comment['created_at'] ?> <i class="fa fa-clock-o" aria-hidden="true"></i></span>
                         </div>
-                        <div class="col-md-10 right-content border border-top-0 border-bottom-0 border-right-0">
-                            <p class="forum-content">
-                                <?= $comment['body'] ?>
-                            </p>
-                        </div>
-                    </div>                    
-                </div>
-                <div class="card-footer">
-                    <a class="btn btn-primary btn-reply float-right" href="#reply">Reply <span> <i class="fa fa-reply" aria-hidden="true"></i></span></a>
-                </div>
-            </div> 
-            <?php endforeach; ?>
-            
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-2 text-center">
+                                <span><img src="<?= $thread['avatar_url'] ?>" class="img-thumbnail w-50 mt-3" aria-hidden="true"></i></span>
+                            </div>
+                            <div class="col-md-10 right-content border border-top-0 border-bottom-0 border-right-0">
+                                <div class="forum-content">
+                                    <?= $comment['body'] ?>
+                                </div>
+                            </div>
+                        </div>                    
+                    </div>
+                    <div class="card-footer footer-reply">
+                        <a class="btn btn-primary btn-reply float-right" href="#reply">Reply <span> <i class="fa fa-reply" aria-hidden="true"></i></span></a>
+                    </div>
+                </div> 
+                <?php endforeach; ?>
+                
 
 
-            <!-- Comments Form -->
-            <div class="card space-between w3-card-4" id="reply">
-                <div class="card-header">
-                    <div class="leave-reply">
-                        <p class="d-inline-block">Leave a Reply</p>
+                <!-- Comments Form -->
+                <div class="card space-between" id="reply">
+                    <div class="card-header">
+                        <div class="leave-reply font-weight-bold">
+                            <span class="d-inline-block">Leave a Reply</span>
+                        </div>
                     </div>
-                </div>
-                <div class="card-block page-wrapper">
-                        <form action="post/reply/<?=$thread['id']?>" method="POST" role="form" class="message">
-                            <textarea name="body" class="form-control" id="reply-system" rows="3"></textarea>
-                            <button type="submit" value="Post Reply" class="btn btn-primary btn-reply float-right" style="margin-top: 10px;"><span> <i class="fa fa-paper-plane" aria-hidden="true"></i></span> Post Reply</button>
-                        </form>
+                    <div class="card-block reply-wrapper">
+                            <form action="post/reply/<?=$thread['id']?>" method="POST" role="form" class="message">
+                                <textarea name="body" class="form-control" id="reply-system" rows="10"></textarea>
+                                <button type="submit" value="Post Reply" class="btn btn-primary btn-reply float-right" style="margin: 10px;"><span> <i class="fa fa-paper-plane" aria-hidden="true"></i></span> Post Reply</button>
+                            </form>
                     </div>
+                    
                 </div>
             </div>
         </div>
