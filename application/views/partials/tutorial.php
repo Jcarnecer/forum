ea6a3c18bd09361c978e955551787e601f758ee5
aereneth <zerify@gmail.com>
2018-02-20 15:30:57 +0800

    Added tutorial

---
+++ application/views/partials/tutorial.php
@@ -0,0 +1,65 @@
+<div id="tutorialModal" class="modal fade" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content" style="box-shadow: 0 0 10px rgba(0,0,0,0.2);">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/welcome.png') ?>" alt="Sidebar" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Welcome to PayakApps' Discussion Module</h5>
+                                <p class="card-text">This is where ideas are born and talked about!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/thread.png') ?>" alt="Dashboard" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Threads</h5>
+                                <p class="card-text">Threads are discussions of specific topics in scope of the project.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/create.png') ?>" alt="Dashboard" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Creating Thread</h5>
+                                <p class="card-text">Create a thread to discuss a particular topic. Add a short description of what will be the thread all about.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/reply.png') ?>" alt="Dashboard" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Replying to Thread</h5>
+                                <p class="card-text">Each project member can share ideas and solution by replying to a thread.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/react.png') ?>" alt="Dashboard" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Reacting to a Thread Reply</h5>
+                                <p class="card-text">Other project member can react to a reply whether it is a positive and helpful one or rather a negative one.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev text-dark" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <i class="fa fa-angle-left fa-3x"></i>
+                    <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span> -->
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next text-dark" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <i class="fa fa-angle-right fa-3x"></i>
+                    <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span> -->
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
