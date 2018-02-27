2d046ead10c994997db62e95aff1fc63fae5d476
aereneth <zerify@gmail.com>
2018-02-27 15:14:30 +0800

    Added files tab

--- application/views/partials/sidebar.php
+++ application/views/partials/sidebar.php
@@ -76,12 +76,15 @@
     </div>
     
     <div class="inner-content d-flex flex-column">
-        <ul class="nav nav-tabs project-buttons">
-            <li class="nav-item w-50">
+        <ul class="nav nav-tabs d-flex flex-nowrap project-buttons">
+            <li class="nav-item w-100">
                 <a href="<?= ENVIRONMENT === 'development' ? 'http://localhost/task/project/' . $project['id'] : 'http://task.payakapps.com//project/' . $project['id'] ?>" class="nav-link project-button" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-tasks"></i> Tasks</a>
             </li>
-            <li class="nav-item w-50">
+            <li class="nav-item w-100">
                 <a href="<?= base_url('/project/' . $project['id']) ?>" onclick="javascript:void(0)" class="nav-link secondary-button active"><i class="fa fa-exchange-alt"></i> Discussion</a>
+            </li>
+            <li class="nav-item w-100">
+                <a href="<?= ENVIRONMENT === 'development' ? 'http://localhost/files/project/' . $project['id'] : 'http://files.payakapps.com//project/' . $project['id'] ?>" class="nav-link secondary-button"  role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-file"></i> Files</a>
             </li>
         </ul>
 
